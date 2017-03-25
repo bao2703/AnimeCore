d373f4552b3e75c0297c3681c00ec2cfb89df2a9
bao2703 <bao2703@gmail.com>
2017-03-25 22:27:46 +0700

    Fix bugs

---
+++ AnimeCore/wwwroot/js/movie-controller.js
@@ -0,0 +1,29 @@
+﻿(function() {
+    "use strict";
+
+    angular.module("app", []).controller("movie-controller", controller);
+
+    controller.$inject = ["$scope", "$http"];
+
+    function controller($scope, $http) {
+        $http.get("https://localhost:44308/api/Movie").then(function(response) {
+            $scope.movies = response.data;
+            $scope.propertyName = "name";
+            $scope.reverse = false;
+            $scope.status = "";
+            $scope.release = "";
+
+            $scope.movies.forEach(function(movies) {
+                if (movies.status === 1) {
+                    movies.status = "Completed";
+                } else {
+                    movies.status = "Ongoing";
+                }
+            });
+        });
+
+        activate();
+
+        function activate() {}
+    }
+})();
