f5d64c05bf9ed2377a5bdebc178dfa86d357ea22
bao2703 <bao2703@gmail.com>
2017-03-26 16:54:49 +0700

    Implement pagination feature

--- AnimeCore/wwwroot/js/movie-controller.js
+++ AnimeCore/wwwroot/js/movie-controller.js
@@ -1,29 +1,34 @@
 ﻿(function() {
     "use strict";
-
-    angular.module("app", []).controller("movie-controller", controller);
+    var app = angular.module("app", ["angularUtils.directives.dirPagination"]);
+    app.controller("movie-controller", controller);
+    app.filter("status",
+        function() {
+            return function(data) {
+                if (data === 1) {
+                    return "Completed";
+                }
+                return "Ongoing";
+            };
+        });
 
     controller.$inject = ["$scope", "$http"];
 
     function controller($scope, $http) {
-        $http.get("https://localhost:44308/api/Movie").then(function(response) {
-            $scope.movies = response.data;
-            $scope.propertyName = "name";
-            $scope.reverse = false;
-            $scope.status = "";
-            $scope.release = "";
+        angular.element(document).ready(function() {
+            $http.get("https://localhost:44308/api/Movie").then(function(response) {
+                $scope.movies = response.data;
+                $scope.propertyName = "name";
+                $scope.reverse = false;
+                $scope.status = "";
+                $scope.release = "";
+                $scope.genreId = "";
 
-            $scope.movies.forEach(function(movies) {
-                if (movies.status === 1) {
-                    movies.status = "Completed";
-                } else {
-                    movies.status = "Ongoing";
-                }
             });
+
+            activate();
+
+            function activate() {}
         });
-
-        activate();
-
-        function activate() {}
     }
 })();
